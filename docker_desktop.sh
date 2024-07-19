cecfa4c7af4cef069dba4c280f04917156c67fa9
Krushnakant Bhattad <krushnakant@deductive>
2024-07-18 18:26:31 -0700

    updates

--- docker_desktop.sh
+++ docker_desktop.sh
@@ -6,4 +6,5 @@ cd hotelReservation
 ./wrk -D exp -t 2 -c 2 -d 30 -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://frontend.hotel-res.svc.cluster.local:5000 -R 2 
 
 # kubectl get pods
+# kubectl describe pods <pod_name>
 # kubectl delete pods --all
