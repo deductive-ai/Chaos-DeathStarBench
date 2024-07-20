cc38afd0c1ce2e1ee32d41c1adb135728e4d3ba3
Krushnakant Bhattad <krushnakant@deductive>
2024-07-19 18:04:49 -0700

    updates

--- docker_desktop.sh
+++ docker_desktop.sh
@@ -1,10 +1,12 @@
-bash ./hotelReservation/build_docker_images.sh 
+bash ./hotelReservation/kubernetes/scripts/build_docker_images.sh 
 kubectl config use-context docker-desktop
 kubectl delete pods --all
 kubectl apply -Rf "./hotelReservation/kubernetes/"
 cd hotelReservation
-./wrk -D exp -t 2 -c 2 -d 30 -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://frontend.hotel-res.svc.cluster.local:5000 -R 2 
 
 # kubectl get pods
 # kubectl describe pods <pod_name>
+
+# ./wrk -D exp -t 2 -c 2 -d 30 -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://frontend.hotel-res.svc.cluster.local:5000 -R 2 
+
 # kubectl delete pods --all
--- kind.sh
+++ kind.sh
@@ -1,4 +1,4 @@
-bash ./hotelReservation/build_docker_images.sh 
+bash ./hotelReservation/kubernetes/scripts/build_docker_images.sh 
 kind delete cluster --name chaos; 
 kind create cluster --name chaos; 
 kubectl cluster-info --context kind-chaos; 
@@ -8,4 +8,7 @@ kubectl apply -Rf "./hotelReservation/kubernetes/"
 
 # kubectl get pods
 # kubectl describe pods <pod_name>
+
+# ./wrk -D exp -t 2 -c 2 -d 30 -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://frontend.hotel-res.svc.cluster.local:5000 -R 2 
+
 # kubectl delete pods --all
